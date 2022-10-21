191f14c147a282c196aa5c74690db519ed277c4f
kowsik04 <kowsik04786@gmail.com>
2022-10-21 14:59:50 +0530

    First commit

---
+++ B33/src/org/tns/program/Arithmeticoperators.java
@@ -0,0 +1,13 @@
+package org.tns.program;
+
+public class Arithmeticoperators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 18, y = 28;
+		System.out.println(x+y);
+		System.out.println(--x);
+		System.out.println(++y);
+	}
+
+}
