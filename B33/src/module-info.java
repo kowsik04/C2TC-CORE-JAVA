5f58951215c489b7db0ab8107d63cf25198d1358
kowsik04 <kowsik04786@gmail.com>
2022-10-21 14:45:37 +0530

    First commit

---
+++ B33/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author kowsik
+ *
+ */
+module B33 {
+}
---
+++ B33/src/org/tns/program/Firstprogram.java
@@ -0,0 +1,10 @@
+package org.tns.program;
+
+public class Firstprogram {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+
+	}
+
+}
