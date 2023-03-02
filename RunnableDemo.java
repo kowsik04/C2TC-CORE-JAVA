d104181e101d5ffbd06cff0ecd0a64a0015af6cf
kowsik04 <113621862+kowsik04@users.noreply.github.com>
2023-03-02 15:06:33 +0530

    Add files via upload

---
+++ RunnableDemo.java
@@ -0,0 +1,23 @@
+//executor class
+package com.tnsif.daythirteen;
+
+public class RunnableDemo {
+	public static void main(String[] args) {
+			
+		Runnable r1 = new Runnable() {
+			@Override
+			public void run() { // anonymous class
+				System.out.println("Runnable with Anonymous Class");
+			}
+		};
+		
+		Runnable r2 = () -> { // lambda expression
+			System.out.println("Runnable with Lambda Expression");
+		};
+		Thread t1=new Thread(r1);
+		t1.start();
+		new Thread(r2).start();		
+		
+		System.out.println("-------------------------------------------------------");
+	}
+}
---
+++ ThreadDemo.java
@@ -0,0 +1,16 @@
+//Executor class
+package com.tnsif.daythirteen;
+
+public class ThreadDemo {
+	public static void main(String[] args)  {
+		ChildThread t1 = new ChildThread(5, "First");
+		ChildThread t2 = new ChildThread(10, "Second");
+
+		t1.start();
+		 t2.start();
+	
+		
+
+		System.out.println("-----------------------End of Main--------------------------");
+	}
+}
